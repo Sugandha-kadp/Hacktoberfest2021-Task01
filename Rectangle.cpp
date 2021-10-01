e0e0229c9aaf061090a7914ecd106c1642e72200
Sugandha <72730060+Sugandha-kadp@users.noreply.github.com>
2021-10-01 23:44:31 +0530

    Create Rectangle.cpp
    Correct this C++ code mistakes.

---
+++ Rectangle.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+class Rectangle
+{
+  private :
+    int length;
+    int width;
+  
+  public : 
+    int setLength(int no);
+    void setWidth(int no);
+    int calcArea();
+};
+
+  void Rectangle::setLength(no)
+  {
+    length = no;
+  }
+
+  void Rectangle::setWidth(no)
+  {
+    width = no
+  }
+
+  int Rectangle::calcArea()
+  {
+    int area = length*width;
+    
+  }
+
+int main()
+{
+  Rectangle r1;
+
+  r1.setLength(10);
+  r1.setWidth(20);
+  
+
+  //print rectangel area
+  cout << r2.calcArea() <<endl;
+  return 0;
+}
