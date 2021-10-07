0107ff45a84f7a2c78f24edc909c3e4ae3f645ca
ashanq <79820774+ashanq@users.noreply.github.com>
2021-10-07 21:03:26 +0530

    Update Rectangle.cpp

--- Rectangle.cpp
+++ Rectangle.cpp
@@ -7,19 +7,19 @@ class Rectangle
     int width;
   
   public : 
-    int setLength(int no);
+    void setLength(int no);
     void setWidth(int no);
     int calcArea();
 };
 
-  void Rectangle::setLength(no)
+  void Rectangle::setLength(int no)
   {
     length = no;
   }
 
-  void Rectangle::setWidth(no)
+  void Rectangle::setWidth(int no)
   {
-    width = no
+    width = no;
   }
 
   int Rectangle::calcArea()
@@ -37,6 +37,6 @@ int main()
    
 
   //print rectangel area
-  cout << r2.calcArea() <<endl;
+  cout << r1.calcArea() <<endl;
   return 0;
 }
